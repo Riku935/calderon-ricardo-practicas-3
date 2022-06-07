d0e9382623afa1364a6d2c02fa7e4d9b2ba3c915
Riku935 <75951958+Riku935@users.noreply.github.com>
2022-06-07 08:13:29 -0500

    Update DonRiku_crud.sql

--- DonRiku_crud.sql
+++ DonRiku_crud.sql
@@ -1,9 +1,9 @@
 INSERT INTO Customer (customer_number, customer_name,member_number) VALUES 
-(1, 'Ricardo', '55555')
-(2, 'Alam', '66666')
-(3, 'Nestor', '77777')
-(4, 'Carla', '88888')
-(5, 'Aldo', '99999')
+(1, 'Ricardo', '55555'),
+(2, 'Alam', '66666'),
+(3, 'Nestor', '77777'),
+(4, 'Carla', '88888'),
+(5, 'Aldo', '99999');
 SELECT * FROM Customer;
 
 UPDATE Customer
@@ -16,20 +16,20 @@ DELETE FROM Customer WHERE customer_number = 1;
 
 
 INSERT INTO Menu (product_id, productname, productprice) VALUES 
-(19, 'Panecillos', 30)
-(20, 'Donas', 30)
-(21, 'Nachos', 30)
-(22, 'Papas con carne', 30)
-(23, 'Galleta', 30)
+(19, 'Panecillos', 30),
+(20, 'Donas', 30),
+(21, 'Nachos', 30),
+(22, 'Papas con carne', 30),
+(23, 'Galleta', 30);
 SELECT * FROM Menu;
 
 
 INSERT INTO Employe (employe_id, employe_name, age, incorporation_date, rfc_key) VALUES
-(1, 'Tobias', 19,'octubre-2021' ,'TOB040608K67')
-(2, 'Tobias', 19,'noviembre-2021' ,'TOB040608K67')
-(3, 'Tobias', 21,'diciembre-2021' ,'TOB040608K67')
-(4, 'Tobias', 21,'enero-2022' ,'TOB040608K67')
-(5, 'Tobias', 22,'febrero-2022' ,'TOB040608K67')
+(1, 'Tobias', 19,'octubre-2021' ,'TOB040608K67'),
+(2, 'Tobias', 19,'noviembre-2021' ,'TOB040608K87'),
+(3, 'Tobias', 21,'diciembre-2021' ,'TOB040608K97'),
+(4, 'Tobias', 21,'enero-2022' ,'TOB040608K57'),
+(5, 'Tobias', 22,'febrero-2022' ,'TOB040608K37');
 SELECT * FROM Employe;
 
 UPDATE Employe
@@ -44,11 +44,11 @@ DELETE FROM Employe WHERE employe_id = 1;
 
 
 INSERT INTO Purchase (transaction_id, employe_id, transaction_date, branch_adress, product_name, total_amount ) VALUES
-(1, 1, NOW(), 'Avenida siempre viva', 'Hamburguesa sencilla','50')
-(2, 1, NOW(), 'Avenida siempre viva', 'Hamburguesa angus')
-(3, 3, NOW(), 'Avenida siempre viva', 'Hamburguesa hawaiana')
-(4, 2, NOW(), 'Avenida siempre viva', 'Hamburguesa vegetariana')
-(5, 5, NOW(), 'Avenida siempre viva', 'aros de cebolla')
+(1, 1, NOW(), 'Avenida siempre viva', 'Hamburguesa sencilla','50'),
+(2, 1, NOW(), 'Avenida siempre viva', 'Hamburguesa angus','50'),
+(3, 3, NOW(), 'Avenida siempre viva', 'Hamburguesa hawaiana','50'),
+(4, 2, NOW(), 'Avenida siempre viva', 'Hamburguesa vegetariana','50'),
+(5, 5, NOW(), 'Avenida siempre viva', 'aros de cebolla','50');
 SELECT * FROM Purchase;
 UPDATE Purchase
   SET
@@ -61,11 +61,11 @@ UPDATE Purchase
 
 
 INSERT INTO Sale (transaction_number, employe_id, transaction_date, total_amount) VALUES
-(1, 1, NOW(), '200')
-(2, 1, NOW(), '250')
-(3, 5, NOW(), '100')
-(4, 3, NOW(), '30')
-(5, 3, NOW(), '100')
+(1, 1, NOW(), '200'),
+(2, 1, NOW(), '250'),
+(3, 5, NOW(), '100'),
+(4, 3, NOW(), '30'),
+(5, 3, NOW(), '100');
 SELECT * FROM Sale;
 UPDATE Sale
   SET
@@ -75,13 +75,13 @@ UPDATE Sale
   WHERE transaction_number = 1;
 
 INSERT INTO Complaints (complaint_id, customer_number, date_id, commentary) VALUES
-(1, 1, NOW(), 'Comentario1')
-(2, 2, NOW(), 'Comentario2')
-(3, 3, NOW(), 'Comentario3')
-(4, 4, NOW(), 'Comentario4')
-(5, 5, NOW(), 'Comentario5')
+(1, 1, NOW(), 'Comentario1'),
+(2, 2, NOW(), 'Comentario2'),
+(3, 3, NOW(), 'Comentario3'),
+(4, 4, NOW(), 'Comentario4'),
+(5, 5, NOW(), 'Comentario5');
 
-SELECT * FROM Complaints WHERE complaint_id = 1;
+SELECT * FROM Complaints WHERE complaint_id = 2;
 
 DELETE FROM Complaints WHERE complaint_id = 1;
 
